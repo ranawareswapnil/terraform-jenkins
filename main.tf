2978df11d9d58030f8b3d90adcd155a724b7d8e9
ranawareswapnil <ranawareswapnil@gmail.com>
2026-06-29 15:39:46 +0530

    Ami added

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region     = "eu-west-1"
+  region     = "us-east-1a"
 
 }
 
 resource "aws_instance" "EC2_first" {
-  ami           = "ami-0442403fb8d244144"
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   tags = {
     Name = "Server1_tf"
